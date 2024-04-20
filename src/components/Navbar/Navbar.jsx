4c546bbb78eee5139547671dc024aef806a34066
somto <somto565@gmail.com>
2024-04-20 12:22:07 +0100

    Add markup for Navbar menu, search icon, basket icon, and sign in button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,20 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <img src={assets.logo} alt="" />
+        <ul className='navbar-menu'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="" />
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="" />
+                <div className="dot"></div>
+            </div>
+            <button>sign in</button>
+        </div>
     </div>
   )
 }
