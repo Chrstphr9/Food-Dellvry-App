eccbf8696c4fec58eb1c31e83bfe5313174a95ff
somto <somto565@gmail.com>
2024-05-15 09:13:42 +0100

    Add react-router-dom for client side routing; add Home, Cart, and Placeorder components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Placeorder from './components/pages/PlaceOrder/Placeorder'
+import Cart from "./components/pages/Cart/Cart"
+import Home from "./components/pages/Home/Home"
 
 const App = () => {
   return (
     <div className='app'>
         <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} /> 
+          <Route path='/cart' element={<Cart />} /> 
+          <Route path='/order' element={<Placeorder />} />
+        </Routes>
     </div>
   )
 }
