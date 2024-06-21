8632d0d6ec83cdb0a500f3a905020a38d6a07a4c
somto <somto565@gmail.com>
2024-06-21 19:25:04 +0100

    link on the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "./Navbar.css"
 import { assets } from "../../assets/assets"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -10,10 +11,10 @@ const Navbar = () => {
     <div className='navbar'>
         <img src={assets.logo} alt="" />
         <ul className='navbar-menu'>
-            <li className={menu === "home"?"active":""}>Home</li>
-            <li className={menu === "menu"? "active":""}>About</li>
-            <li className={menu === "mobile-app"? "active":""}>Mobile App</li>
-            <li className={menu === "contact-us"? "active":""}>Contact Us</li>
+            <Link onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>Home</Link>
+            <Link onClick={()=>setMenu("menu")} className={menu === "menu"? "active":""}>About</Link>
+            <Link onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"? "active":""}>Mobile App</Link>
+            <Link onClick={()=>setMenu("contact")} className={menu === "contact-us"? "active":""}>Contact Us</Link>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
