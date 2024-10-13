d3ff5403869dea393c2d112036b2d9dbe08b3d61
somto <somto565@gmail.com>
2024-10-13 23:27:10 +0100

    Wrap App component with AuthContextProvider and React.StrictMode, remove BrowserRouter

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext';
 import StoreContextProvider from './context/StoreContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <AuthContextProvider>
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>,
 )
