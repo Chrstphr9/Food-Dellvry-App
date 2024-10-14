cd416fe5d9ec1f3561e9648715f9db52987f741b
somto <somto565@gmail.com>
2024-10-14 20:21:10 +0100

    Removed Home import and replaced with HomeX, commented out original Home import, and updated route path to use HomeX component.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,9 +3,10 @@ import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Placeorder from './components/pages/PlaceOrder/Placeorder'
 import Cart from "./components/pages/Cart/Cart"
-import Home from "./components/pages/Home/Home"
+// import Home from "./components/pages/Home/Home"
 import Footer from './components/Footer/Footer'
-import LoginPopup from './components/LoginPopup/LoginPopup'
+import LoginPopup from './components/LoginPopup/LoginPopup' 
+import HomeX from './components/pages/HomeX/HomeX'
 
 const App = () => {
 
@@ -17,8 +18,8 @@ const App = () => {
     
     <div className='app'>
         <Navbar setShowLogin={setShowLogin}/>
-        <Routes>
-          <Route path='/' element={<Home />} /> 
+        <Routes> 
+          <Route path='/' element={<HomeX />} /> 
           <Route path='/cart' element={<Cart />} /> 
           <Route path='/order' element={<Placeorder />} />
         </Routes>
