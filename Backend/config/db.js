f1fda0df4fb4d1e55df7330ab198202eab058d15
somto <somto565@gmail.com>
2024-10-29 08:28:07 +0100

    Updated MongoDB connection string, fixed typo in foodController, and updated package-lock.json and package.json with new MongoDB version

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const ConnectDB = async () => {
-    await mongoose.connect('mongodb+srv://octan311:AIqzFwuQfz0z5YSl@cluster0.qes2c.mongodb.net/blog-app')
+    await mongoose.connect('mongodb+srv://somto565:test1234@cluster21.p6skm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster21')
     console.log("DB Connected");
     
 }
