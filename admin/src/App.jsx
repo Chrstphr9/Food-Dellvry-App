3ca4417ca0d3fb233b80aa6116f79430308e1c3a
somto <somto565@gmail.com>
2024-11-04 10:56:33 +0100

    Added Navbar component and BrowserRouter to App component, and removed StrictMode

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./App.css"
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+      </div>
     </div>
   )
 }
