620387c9f2d83fbfe65adfb3e0a086caa110ebc1
somto <somto565@gmail.com>
2024-11-05 08:25:45 +0100

    able to display image

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Add.css'
 import { assets } from '../../assets/assets'
 
 const Add = () => {
+
+    const [image, setImage] = useState(false)
+
+
   return (
     <div>
         <div className="add">
@@ -10,9 +14,9 @@ const Add = () => {
                 <div className="add-img-upload flex-col">
                     <p>Upload Image</p>
                     <label htmlFor="image">
-                        <img src={assets.upload_area} alt="" />
+                        <img src={image?URL.createObjectURL(image):assets.upload_area} alt="" />
                     </label>
-                    <input type="file" id='image' hidden required />
+                    <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required />
                 </div>
                 <div className="add-product-name flex-col">
                     <p>Product Name</p>
