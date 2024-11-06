501384d71590ecd21309c94aa78517e82e62bc8d
somto <somto565@gmail.com>
2024-11-06 10:10:19 +0100

    Added ToastContainer from react-toastify to App component

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -27,7 +27,7 @@ const Add = () => {
         formData.append("description",data.description)
         formData.append("price",Number(data.price))
         formData.append("category",data.category)
-        formData.append("image",image)
+        formData.append("image",dimage)
         const response = await axios.post(`${url}/api/food/add`, formData, )
         if (response.data.success) {
             setData(
