f0122b0bf7ae65e72da5d1c280526c58c410572e
somto <somto565@gmail.com>
2024-11-16 20:42:58 +0100

    Removed image file, updated App.jsx to pass URL prop to components, added CSS for list table, and modified Add and List components to use URL prop and implement remove food functionality.

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -4,15 +4,14 @@ import axios from 'axios'
 import { toast } from 'react-toastify'
 import { useEffect } from 'react'
 
-const List = () => {
+const List = ({url}) => {
 
-  const url = 'http://localhost:4000'
+  
   const [list, setList] = useState([])
 
   const fetchList = async () =>{
     const response = await axios.get(`${url}/api/food/list`);
-    console.log(response.data);
-    
+  
     if (response.data.success) {
       setList(response.data.data)
     }
@@ -20,6 +19,17 @@ const List = () => {
       toast.error("Error")
     }
   } 
+
+  const removeFood = async(foodId) => {
+    const response = await axios.post(`${url}/api/food/remove`,{id:foodId})
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message)
+    } else {
+      toast.error("Error");
+    }
+    
+  }
 
   useEffect(() => {
     fetchList();
@@ -45,7 +55,7 @@ const List = () => {
                 <p>{item.name}</p>
                 <p>{item.category}</p>
                 <p>${item.price}</p>
-                <p>X</p>
+                <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
               </div>
             )
         })}
