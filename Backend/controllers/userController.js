2b86a6b09397fec4ad3538e792b81c118d6a882f
somto <somto565@gmail.com>
2024-11-18 19:45:16 +0100

    Added JWT secret to .env file, fixed typo in userController.js, implemented user registration with email validation and password hashing, and added userRouter to server.js

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,51 @@
+import userModel from "../models/userModel.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+// login user
+const loginUser = async (req, res) => {
+
+} 
+
+const createToken = (id) => {
+    return jwt.sign({id},)
+}
+
+// register 
+const registerUser = async (req, res) => {
+    const {name, password, email} = req.body;
+    try {
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.json({success:false, message:"User already exists"})
+        }
+
+        //validating email and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message:"Please enter a valid email"})
+        }
+
+        if (password.length<8) {
+            return res.json({success:false, message:"Please enter strong password"})
+        }
+
+        //hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            passowrd:hashedPassword
+        })
+
+        const user = await newUser.save()
+
+
+    } catch (error) {
+        
+    }
+}
+
+export {loginUser, registerUser}
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { ConnectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
 
 
 //app config
@@ -19,6 +20,7 @@ ConnectDB();
 //api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
