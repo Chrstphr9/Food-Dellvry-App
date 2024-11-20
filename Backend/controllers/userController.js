d2a4688ac3a64832349710730f051d270a4932c1
somto <somto565@gmail.com>
2024-11-20 22:10:42 +0100

    Added password comparison using bcrypt and token creation in loginUser function

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -11,8 +11,22 @@ const loginUser = async (req, res) => {
         if (!user) {
             return res.json({success:false, message:"User Doesn't Exist"})
         }
-    } catch(error) {
+        console.log("Password from request body:", password);
 
+
+        
+        const isMatch = await bcrypt.compare(password, user.password)   
+
+        if (!isMatch) {
+            return res.json({success:false, message:"Invalid Credentials"})
+        }
+
+        const token = createToken(user._id)
+        res.json({success:true, token})
+
+    } catch(error) {
+        console.log(error);
+        res.json({success:false, message:"Error"})
     }
 
 };
