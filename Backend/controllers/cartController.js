84c9dbc7f46def5ae0d587a4ac84072ba63c0900
somto <somto565@gmail.com>
2024-11-21 08:18:43 +0100

    Added cartController.js with three empty functions: addToCart, removefromCart, and getCart

---
+++ Backend/controllers/cartController.js
@@ -0,0 +1,18 @@
+import userModel from "../models/userModel.js"
+
+// Add items to user cart
+const addToCart = async(req,res) => {
+
+}
+
+// Remove item from user cart
+const removefromCart  = async(req,res) => {
+
+}
+
+// Fetch user cart data
+const getCart = async (req,res) => {
+
+}
+
+export {addToCart, removefromCart, getCart}
