10dde17c74a85914074fd4cefee47b97fef9f8f1
somto <somto565@gmail.com>
2024-11-25 18:49:18 +0100

    Updated Stripe secret key, added Stripe payment integration to order controller, and modified Place Order page to collect user information and submit order

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,11 +1,13 @@
-import orderModel from "../models/orderModel";
-import userModel from "../models/userModel"
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js"
 import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing user order for frontend
 const placeOrder = async (req, res) => {
+
+    const frontend_url = 'hhtp://localhost:5173'
 
         try {
             const newOrder = new orderModel({
@@ -17,10 +19,40 @@ const placeOrder = async (req, res) => {
             await newOrder.save();
             await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}})
 
-            
+            const line_items = req.body.items.map((item)=> ({
+                price_data:{
+                    currency:"Naira",
+                    product_data: {
+                        name: item.name,
+                    },
+                    unit_amount: item.price*100*1240,
+                },
+                quantity:item.quantity
+            }))
 
+            line_items.push({
+                price_data:{
+                    currency:"Naira",
+                    product_data:{
+                        name:"Delivery Charge"
+                    },
+                    unit_amount:2*100*1240
+                },
+                quantity:1
+            })
+
+            const session = await stripe.checkout.sessions.create({
+                line_items:line_items,
+                mode:'payment',
+                success_url:`${frontend_url}/verify?success=true&orderId${newOrder._id}`,
+                cancel_url:`${frontend_url}/verify?success=false&orderId${newOrder._id}`,
+            })
+
+            res.json({success:true, session_url:session.url})
 
         } catch (error) {
+            console.log(error);
+            res.json({success:false,message:"Error"})
             
         }
 }
--- Frontend/src/pages/PlaceOrder/Placeorder.jsx
+++ Frontend/src/pages/PlaceOrder/Placeorder.jsx
@@ -1,30 +1,62 @@
 import { useContext } from 'react'
 import "./Placeorder.css"
 import { StoreContext } from '../../context/StoreContext'
+import { useState } from 'react';
 
 const Placeorder = () => {
 
-  const {getTotalCartAmount} = useContext(StoreContext)
+  const {getTotalCartAmount, token,food_list,cartItem,url} = useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    street:"",
+    city:"", 
+    state:"",
+    zipcode:"",
+    country:"",
+    phone:""
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value
+    setData(data=>({...data,[name]:value}))
+  }
+
+  const placeOrder = async (event) =>{
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item)=> {
+      if (cartItem[item._id]>0) {
+        let itemInfo = item;
+      } else {
+        
+      }
+    })
+  }
+
 
   return (
-    <form className='place-order'>
+    <form onSubmit={placeOrder} className='place-order'>
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder='First name'/>
-          <input type="text" placeholder='Last name'/>
+          <input name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First name'/>
+          <input name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last name'/>
         </div>
-        <input type="email" placeholder='Email address' />
-        <input type="text" placeholder='Street'/>
+        <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Email address' />
+        <input name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='Street'/>
         <div className="multi-fields">
-          <input type="text" placeholder='City'/>
-          <input type="text" placeholder='State'/>
+          <input name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City'/>
+          <input name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State'/>
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='ZipCode'/>
-          <input type="text" placeholder='Country'/>
+          <input name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='ZipCode'/>
+          <input name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country'/>
         </div>
-        <input type="text" placeholder='Phone number' />
+        <input name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone number' />
       </div>
       <div className="place-order-right">
       <div className="cart-total">
@@ -45,7 +77,7 @@ const Placeorder = () => {
                 <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
           </div>
-          <button>PROCEED TO PAYMENT</button>
+          <button type='submit'>PROCEED TO PAYMENT</button>
         </div>
       </div>
     </form>
