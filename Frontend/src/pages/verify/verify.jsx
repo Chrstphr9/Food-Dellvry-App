80ea3c38a70d803e51418f9b9bb974ca63b41cf6
somto <somto565@gmail.com>
2024-11-26 00:52:07 +0100

    Added CSS for verify page, including spinner animation, and updated verify.jsx to import StoreContext and useContext hook.

--- Frontend/src/pages/verify/verify.jsx
+++ Frontend/src/pages/verify/verify.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './verify.css'
 import { useSearchParams } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const verify = () => {
 
     const [searchParams, setSearchParams ] = useSearchParams();
     const success = searchParams.get("success")
     const orderId = searchParams.get("orderId")
+    const {url} = useContext(StoreContext)
 
-    console.log(success,orderId);
+    
 
   return (
     <div className='verify '>
         <div className="spinner">
-            
+
         </div>
     </div>
   )
