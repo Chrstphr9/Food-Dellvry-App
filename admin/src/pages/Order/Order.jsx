4f0e0ab8d04cfcf65b458f226d64a9ec7ac1c462
somto <somto565@gmail.com>
2024-11-26 12:38:59 +0100

    Added imports, state, and API call to fetch orders in Order component

--- admin/src/pages/Order/Order.jsx
+++ admin/src/pages/Order/Order.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 import './Order.css'
+import { useState } from 'react'
+import { toast } from "react-toastify"
+import { useEffect } from 'react'
+import axios from 'axios'
+const Order = ({url}) => {
 
-const Order = () => {
+  const [orders, setOrders] = useState([]);
+
+    const fetchAllOrders = async () => {
+      const response = await axios.get(url+"/api/order/list");
+      if (response.data.success) {
+        setOrders(response.data.data)
+          
+      } else {
+        toast.error("Error")
+      }
+    }
+
+    useEffect(() => {
+      fetchAllOrders();
+    }, [])
+    
+  
+
+
   return (
     <div>Order</div>
   )
