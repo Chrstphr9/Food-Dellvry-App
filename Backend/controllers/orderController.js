07431d77e729b65a3b0d0cd8292882bf6fd7c2d4
somto <somto565@gmail.com>
2024-11-26 12:19:00 +0100

    Updated orderController.js to add try-catch block in listOrders function and removed authMiddleware from listOrders route in orderRoute.js

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -87,8 +87,14 @@ const userOrders = async (req,res) => {
 }
 
 // Listing Orders for Admin panel
-const listOrders = async (req,res) = {
-
+const listOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true, data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 }
 
 export {placeOrder, verifyOrder, userOrders, listOrders}
