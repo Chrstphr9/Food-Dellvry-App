9db8b7de436bc90bee2bb8e677c4b1e2c0673e62
somto <somto565@gmail.com>
2024-11-26 02:58:30 +0100

    Added assets import and updated MyOrders component with new UI elements and order data rendering

--- Frontend/src/pages/MyOrders/MyOrders.jsx
+++ Frontend/src/pages/MyOrders/MyOrders.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import "./MyOrders.css"
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios'
+import { assets } from "../../assets/assets";
 
 const MyOrders = () => {
 
@@ -21,8 +22,28 @@ const MyOrders = () => {
     
 
   return (
-    <div>
-
+    <div className='my-orders'>
+        <h2>My Orders</h2>
+        <div className="container">
+            {data.map((order, index)=> {
+                return (
+                    <div key={index} className='my-order-order'>
+                        <img src={assets.parcel_icon} alt="" />
+                        <p>{order.items.map((item, index)=> {
+                            if (index=== order.items.length-1) {
+                                return item.name+"x"+item.quantity
+                            } else {
+                                return item.name+"x"+item.quantity+","
+                            }
+                        })}</p>
+                        <p>${order.amount}.00</p>
+                        <p>Items: {order.items.length}</p>
+                        <p><span>&#x25cf;</span><b>{order.status}</b></p>
+                        <button>Track Order</button>
+                    </div> 
+                )
+            })}
+        </div>
     </div>
   )
 }
