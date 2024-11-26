1a9b667dbf67b6af2078577408535fe31e0cc118
somto <somto565@gmail.com>
2024-11-26 22:17:03 +0100

    Added statusHandler function and integrated it with select element in Order component

--- admin/src/pages/Order/Order.jsx
+++ admin/src/pages/Order/Order.jsx
@@ -20,6 +20,18 @@ const Order = ({ url }) => {
     }
   }
 
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url+"/api/order/status", {
+      orderId,
+      status:event.target.value
+    })
+    if (response.data.success) {
+      await fetchAllOrders();
+    } else {
+      
+    }
+  }
+
   useEffect(() => {
     fetchAllOrders();
   }, [])
@@ -53,7 +65,7 @@ const Order = ({ url }) => {
             </div>
             <p>Items:{order.items.length}</p>
             <p>${order.amount}</p>
-            <select>
+            <select onChange={(event)=>statusHandler(event, order._id)} value={order.status}>
               <option value="Food Prcessing">Food Prcessing</option>
               <option value="Out For Delivery">Out For Delivery</option>
               <option value="Delivered">Delivered</option>
